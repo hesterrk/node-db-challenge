2fe5450768e77f30887e0ef455983aa9ddd06440
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-20 08:23:44 +0000

    tasks table

--- data/migrations/20200320082122_projects.js
+++ data/migrations/20200320082122_projects.js
@@ -8,7 +8,30 @@ exports.up = async function(knex) {
       .boolean("completed")
       .notNull()
       .defaultTo(false);
-  });
+  })
+
+  //Tasks Table 
+
+await knex.schema.createTable("tasks", (table) => {
+    table.increments("id")
+    table.text("description").notNull()
+    table.text("notes")
+    table.integer("project_id")
+    .unsigned()
+    .notNull()
+    .references("id")
+    .inTable("projects")
+    .onUpdate("CASCADE")
+    .onDelete("CASCADE")
+    table.boolean("completed").notNull().defaultTo(false)
+})
+
+
+
+
+
+
+
 };
 
 exports.down = async function(knex) {};
