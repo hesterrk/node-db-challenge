336c35962924663893b3facd49d9f49adcf4a353
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-20 09:20:21 +0000

    stretch editing

--- tasks/tasks-model.js
+++ tasks/tasks-model.js
@@ -7,29 +7,27 @@ module.exports = {
 };
 
 function getTasks() {
-    return db("tasks as t")
-      .join("projects as p", "p.id", "t.project_id")
-      .select(
-        "p.name as projectname",
-        "p.description as projectdescription",
-        "t.id as task_id",
-        "t.description as task_description",
-        "t.notes as task_notes",
-        "t.project_id",
-        "t.completed as task_completed"
-      );
-  }
-  
-  function getTaskById(id) {
-    return db("tasks")
-      .where({ id })
-      .first();
-  }
-  
-  
-  //ADD TASK HERE 
-  
-  function addTask(task) {
-    return db("tasks").insert(task)
-    
-  }
+  return db("tasks as t")
+    .join("projects as p", "p.id", "t.project_id")
+    .select(
+      "p.name as projectname",
+      "p.description as projectdescription",
+      "t.id as task_id",
+      "t.description as task_description",
+      "t.notes as task_notes",
+      "t.project_id",
+      "t.completed as task_completed"
+    );
+}
+
+function getTaskById(id) {
+  return db("tasks")
+    .where({ id })
+    .first();
+}
+
+//ADD TASK HERE
+
+function addTask(task) {
+  return db("tasks").insert(task);
+}
