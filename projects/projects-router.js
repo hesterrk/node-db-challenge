d5387e10b7afa7681d52f318d61e99e438b66802
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-20 08:47:33 +0000

    gets

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -8,6 +8,37 @@ const router = express.Router();
 router.use('/:id/projectresources', projresourceRouter)
 
 
+//GET ALL PROJECTS
+
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await projects.getProjects())
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET PROJECT BY ID 
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const project = await projects.getProjectById(id);
+    if (project) {
+      res.json(project);
+    } else {
+      res.status(404).json({ message: "Could not find project with given id." });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+
+
 
 
   module.exports = router;
